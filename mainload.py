9291623212d46787d73ed54ee383546863489a66
Mr. BigDog <100119633+OldTownRoadHorse@users.noreply.github.com>
2023-08-13 23:56:28 +0000

    Update mainload.py

--- mainload.py
+++ mainload.py
@@ -19,7 +19,7 @@ def dlod():
         qs = input("Если хотите скачать нажмите ENTER, или нет нажмите N+ENTER: ")
         if qs == "":
             video1 = video.streams.get_highest_resolution()
-            pht = "D:\Videos\YuLoader"
+            pht = "C:\Videos\YuLoader"
             video1.download(pht)
             qs2 = input("Хотите скачать что-нибудь ещё? ENTER - да | N + ENTER - нет: ")
             if qs2 == "":
